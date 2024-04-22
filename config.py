2cc9cd024c478a45cd2c7ac7765299b4d4029787
kirya-droid <kirya54@gmail.com>
2024-04-22 20:03:58 +0300

    deleting the previos version

--- config.py
+++
@@ -1,8 +0,0 @@
-def get_token_from_file(filename):
-    try:
-        with open(filename, 'r') as file:
-            token = file.readline().strip()
-        return token
-    except FileNotFoundError:
-        print(f"Пользователь {filename} не найден.")
-        return None
